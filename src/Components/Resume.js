7ea784f1a851214e3084fef229f56713ebb84edd
PatelShalin <patel.shalin@outlook.com>
2021-11-29 22:26:29 -0500

    bug fix

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render() {
     if (this.props.data) {
-      var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education) {
         return (
           <div key={education.school}>
@@ -53,16 +52,6 @@ class Resume extends Component {
           </div>
         );
       });
-      var skills = this.props.data.skills.map(function(skills) {
-        var className = 'bar-expand ' + skills.name.toLowerCase();
-        return (
-          <li key={skills.name}>
-            <span style={{ width: skills.level }} className={className}></span>
-            <em>{skills.name}</em>
-          </li>
-        );
-      });
-    }
 
     return (
       <section id="resume">
